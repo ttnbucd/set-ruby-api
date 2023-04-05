3608acfafbfdd38d044a9fa6bc84307d4b1f2365
内田達延 <uchida@interg.co.jp>
2023-04-05 11:25:37 +0900

    [wip]

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/', async (req: Request, res: Response) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   const inputText = req.body.text ?? null;
   if (inputText) {
     const parsedTexts = parseJapanese(inputText);
